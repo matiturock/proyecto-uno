215203aa8c0988551335587d83b7bd0891b8a863
Matías Almirón <matias.almiron@innew.la>
2026-02-05 11:37:58 -0300

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true, // Permite usar describe, test, expect sin importar
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js', // Archivo de configuración
-  },
 });
